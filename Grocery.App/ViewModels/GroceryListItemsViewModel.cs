785275ec1facc109b987e2c45b1367e18244e872
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2025-09-15 16:26:51 +0200

    push zodat ik tijdens kijken wat het probleem is niet alles verloren ben
    error:  <Grocery.App>\ViewModels\GroceryListItemsViewModel.cs:215 Cannot resolve symbol 'AndroidX'

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -4,6 +4,7 @@ using Grocery.App.Views;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
+using AndroidX.Fragment.App.StrictMode;
 
 namespace Grocery.App.ViewModels
 {
@@ -36,10 +37,10 @@ namespace Grocery.App.ViewModels
         {
             AvailableProducts.Clear();
             List<Product> products = _productService.GetAll();
-            foreach (var product in products)
+            foreach (var p in products)
             {
-                if (product.Stock > 0 && !AvailableProducts.Contains(product))
-                    AvailableProducts.Add(product);
+                if (p.Stock > 0 && MyGroceryListItems.All(g => g.ProductId != p.Id))
+                    AvailableProducts.Add(p);
             }
         }
 
@@ -57,12 +58,14 @@ namespace Grocery.App.ViewModels
         [RelayCommand]
         public void AddProduct(Product product)
         {
-            //Controleer of het product bestaat en dat de Id > 0
-            //Maak een GroceryListItem met Id 0 en vul de juiste productid en grocerylistid
-            //Voeg het GroceryListItem toe aan de dataset middels de _groceryListItemsService
-            //Werk de voorraad (Stock) van het product bij en zorg dat deze wordt vastgelegd (middels _productService)
-            //Werk de lijst AvailableProducts bij, want dit product is niet meer beschikbaar
-            //call OnGroceryListChanged(GroceryList);
+            if (product == null)
+                return;
+            GroceryListItem item = new (0,groceryList.Id, product.Id, 1);
+            _groceryListItemsService.Add(item);
+            product.Stock = product.Stock -= 1;
+            _productService.Update(product);
+            AvailableProducts.Remove(product);
+            OnGroceryListChanged(GroceryList);
         }
     }
 }
